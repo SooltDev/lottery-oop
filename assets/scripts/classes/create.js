be69f74c13ffa22b8e5eb9fe3217e9853d1f9182
SooltDev <112691381+SooltDev@users.noreply.github.com>
2023-03-02 17:54:57 +0100

    Update create.js

--- assets/scripts/classes/create.js
+++ assets/scripts/classes/create.js
@@ -8,14 +8,37 @@ class Create {
      * @returns 
      */
     createElement(o, body) {
-        let element = document.createElement(o.tag);
+        this.element = document.createElement(o.tag);
+        
         element.setAttribute("id", o.id)
         element.setAttribute("class", o.cls);
+        
+        let attrs = o.attributes || {}
+        for (const key in attrs)
+            element.setAttribute(key, attrs[key]);
+
         if (o.datakey)
             element.setAttribute(o.datakey, o.dataval);
         if (body)
             element.innerText = body;
+        
+        
 
         return element;
     }
+    
+    appendChild(child){
+        this.element(child.element)
+    }
 }
+
+let newElement = new Create({
+    id: "lista",
+    tag: "ul",
+    parentElement: "#content" // selector ebbe rendereli bele a peldanyt
+    attributes:{
+        name: "sajatlista"
+    },
+    children: [
+    ]
+})
